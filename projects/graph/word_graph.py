6720189c839343d7df4d87bafe08bb74fd11370e
Jonathan Scott <jonathan.scott@gmail.com>
2020-01-21 17:31:07 -0800

    testing on word graph

--- projects/graph/word_graph.py
+++ projects/graph/word_graph.py
@@ -6,8 +6,18 @@ f.close()
 words_set = set(words_list)
 alphabet = list(string.ascii_lowercase)
 
+graph = Graph()
+def find_words(word, graph_words=None):
+    for i in range(len(word)):
+        for letter in alphabet:
+            test_list = list(word)
+            test_list[i] = letter
+            test_word = "".join(test_list)
+            if test_word in words_set:
+                graph_words.add(test_word)
+                graph.add_vertex(test_word)
+
 def shortest_word_transformation(starting_word, ending_word):
-    graph = Graph()
     graph_words = set()
     for i in range(len(starting_word)):
         for letter in alphabet:
@@ -17,6 +27,7 @@ def shortest_word_transformation(starting_word, ending_word):
             if test_word in words_set:
                 graph_words.add(test_word)
                 graph.add_vertex(test_word)
+            find_words(test_word, graph_words)
     for i in range(len(ending_word)):
         for letter in alphabet:
             test_list = list(ending_word)
@@ -25,10 +36,15 @@ def shortest_word_transformation(starting_word, ending_word):
             if test_word in words_set:
                 graph_words.add(test_word)
                 graph.add_vertex(test_word)
+            find_words(test_word, graph_words)
+            # find_words(ending_word, graph_words)    
+    
     for word in graph_words:
         for check_word in graph_words:
             if sum(a != b for a, b in zip(word, check_word)) == 1:
                 graph.add_edge(word, check_word)
     return graph.bfs(starting_word, ending_word)
             
-print(shortest_word_transformation('hit', 'cog'))
+# ['sail', 'bail', 'boil', 'boll', 'bolt', 'boat']
+# print(shortest_word_transformation('hit', 'cog'))
+print(shortest_word_transformation('sail', 'boat'))
